5eda583e5d41e067a23dd6a48b6cd1cbd283d5ca
Sariah Campopiano <scampopiano925@gmail.com>
2022-08-02 22:48:09 -0500

    Trying to get public cards to render to DOM on publicTeams page

--- pages/teams/publicTeams.js
+++ pages/teams/publicTeams.js
@@ -1,19 +1,30 @@
-import React from 'react';
-// import { getPublicTeams } from '../../api/teamsData';
-// import TeamCards from '../../components/cards/TeamCards';
-// import { useAuth } from '../../utils/context/authContext';
+import React, { useEffect, useState } from 'react';
+import { getPublicTeams } from '../../api/teamsData';
+import TeamCards from '../../components/cards/TeamCards';
 
 export default function PublicTeams() {
+  const [publicTeams, setPublicTeams] = useState([]);
+
+  const getAllPublicTeams = () => {
+    getPublicTeams().then((publicTeamsArray) => {
+      setPublicTeams(publicTeamsArray);
+    });
+  };
+
+  useEffect(() => {
+    getAllPublicTeams();
+  }, []);
+
   return (
     <>
       <header>
         <h1 className="title">Public Teams</h1>
       </header>
-      {/* <div className="cards-container">
+      <div className="cards-container">
         {publicTeams?.map((publicTeam) => (
-          <TeamCards key={team.firebaseKey} teamObj={team} user={user} onUpdate={getAllPublicTeams} />
+          <TeamCards key={publicTeam.firebaseKey} publicTeamObj={publicTeam} onUpdate={getAllPublicTeams} />
         ))}
-      </div> */}
+      </div>
     </>
   );
 }
