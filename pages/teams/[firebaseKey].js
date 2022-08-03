a50b46a5c732edf11916a4f1a24c274cfa77f808
Sariah Campopiano <scampopiano925@gmail.com>
2022-08-03 14:51:12 -0500

    Once View is clicked on a public team, the 'add a player' btn found when user clicks View on one of their teams, no longer renders, but it does if the teams and players belong to the user

--- pages/teams/[firebaseKey].js
+++ pages/teams/[firebaseKey].js
@@ -5,11 +5,13 @@ import Link from 'next/link';
 import { Button } from 'react-bootstrap';
 import { viewTeamDetails } from '../../api/mergedData';
 import PlayerCards from '../../components/cards/PlayerCards';
+import { useAuth } from '../../utils/context/authContext';
 
 export default function ViewTeam() {
   const [teamDetails, setTeamDetails] = useState({});
   const router = useRouter();
   const { firebaseKey } = router.query;
+  const { user } = useAuth();
 
   const playersForThisTeam = () => {
     viewTeamDetails(firebaseKey).then(setTeamDetails);
@@ -23,9 +25,11 @@ export default function ViewTeam() {
     <div>
       <header>
         <h1 className="players-with-team">Players of {teamDetails.name}</h1>
+        {user.uid === teamDetails.uid && (
         <Link href="/players/new" passHref>
           <Button variant="warning" className="addPlayerBtn">Add A Player to {teamDetails.name}</Button>
         </Link>
+        )}
       </header>
       <div className="cards-container">
         {teamDetails.players?.map((player) => (
