78a2e56db0905b6cac41e5839a3cffc8a1ec9b17
Sariah Campopiano <scampopiano925@gmail.com>
2022-08-10 15:01:16 -0500

    Trying to figure out how to render only favorite players to the home page

---
+++ components/Favorite.js
@@ -0,0 +1,39 @@
+import React, { useState } from 'react';
+// import PropTypes from 'prop-types';
+import { getPlayers } from '../api/playersData';
+import { useAuth } from '../utils/context/authContext';
+import PlayerCards from './cards/PlayerCards';
+
+function FavoritePlayers() {
+  const [fav, setFav] = useState({});
+  const { user } = useAuth();
+
+  const getAllPlayers = () => {
+    getPlayers(user.uid);
+    console.warn(getAllPlayers, 'Please be what I want');
+  };
+
+  const favoritePlayer = () => {
+    const results = getAllPlayers?.filter((favPlayer) => favPlayer.favorite === true);
+    setFav(results);
+  };
+
+  return (
+    <div>
+      <PlayerCards key={fav?.firebaseKey} playerObj={fav} onUpdate={favoritePlayer} />
+    </div>
+  );
+}
+
+// FavoritePlayers.propTypes = {
+//   players: PropTypes.arrayOf(PropTypes.shape({
+//     firebaseKey: PropTypes.string,
+//     favorite: PropTypes.bool,
+//   })),
+// };
+
+// FavoritePlayers.defaultProps = {
+//   players: {},
+// };
+
+export default FavoritePlayers;
--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,36 @@
+/* eslint-disable react-hooks/exhaustive-deps */
+import { useEffect, useState } from 'react';
+import { getPlayers } from '../api/playersData';
+import PlayerCards from '../components/cards/PlayerCards';
 import { useAuth } from '../utils/context/authContext';
 
 function Home() {
+  const [fav, setFav] = useState([]);
   const { user } = useAuth();
+
+  const getAllPlayers = () => {
+    getPlayers(user.uid).then(setFav);
+  };
+
+  const favoritePlayer = () => {
+    const results = fav?.filter((favPlayer) => favPlayer.favorite === true);
+    setFav(results);
+  };
+
+  useEffect(() => {
+    getAllPlayers();
+  }, []);
 
   return (
     <div>
       <header className="home-title">
         <h1>Ciao, {user.displayName}!</h1>
       </header>
+      <section>
+        {fav?.map((favPlayer) => (
+          <PlayerCards key={favPlayer?.firebaseKey} playerObj={favPlayer} onUpdate={favoritePlayer} />
+        ))}
+      </section>
     </div>
   );
 }
